25183e2b94726c62934d3d7879d60c6c6a2cf3f4
showmikgupta <showmikgupta@gmail.com>
2017-10-05 17:00:16 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,24 @@
-package Model;
+package model;
 
 public class Calculator {
+	private String firstNumber;
+	private String secondNumber;
+	private String operator	;
+	private String answer;
+	private State readingFirst = new ReadingFirstOnly();
+	private State secondOnly = new ReadingSecondOnly();
+	private State currentState = readingFirst;
+	
+	public class ReadingFirstOnly implements State{
+		
+	}
+	
+	public class ReadingSecondOnly implements State{
+		
+	}
+	
+	public class Error implements State{
+		
+	}
 
 }
